5f6ec9c188c6afc5c18828efcc02ff905233a04c
Dipl.-Ing. (FH) Michael Czybor <contact@log1cworx.at>
2023-09-15 09:18:06 +0200

    ADD drawing area events

--- include/gui/drawing_area.h
+++ include/gui/drawing_area.h
@@ -9,5 +9,33 @@ typedef struct _drawing_area
 	uint32_t id;
 } *drawing_area_t;
 
+typedef enum
+{
+	DAE_DRAW,
+	DAE_MOUSE_CLICK_LEFT
+} drawing_area_event_type_t;
+
+typedef struct _draw_event
+{
+	GtkDrawingArea* drawing_area;
+	cairo_t* cr;
+	uint32_t width;
+	uint32_t height;
+} draw_event_t;
+
+typedef struct _mouse_left_click_event
+{
+	GtkDrawingArea* drawing_area;
+	double x;
+	double y;
+	uint32_t n;
+} mouse_left_click_event_t;
+
+typedef union _drawing_area_event
+{
+	draw_event_t draw;
+	mouse_left_click_event_t mouse_left_click_event;
+} *drawing_area_event_t;
+
 GtkWidget* drawing_area_create(uint32_t id, uint32_t width, uint32_t height, void* user_data);
 void drawing_area_destroy(GtkDrawingArea* drawing_area);
--- src/drawing_area.c
+++ src/drawing_area.c
@@ -1,6 +1,6 @@
 #include "gui/drawing_area.h"
 
-extern void drawing_area_callback(cairo_t* cr, int width, int height, drawing_area_t data) __attribute__((weak));
+extern void drawing_area_callback(drawing_area_event_type_t type, drawing_area_t data, drawing_area_event_t e) __attribute__((weak));
 
 drawing_area_t _drawing_area_get_core(GtkDrawingArea* drawing_area);
 
@@ -8,7 +8,27 @@ void _drawing_area_draw(GtkDrawingArea* drawing_area, cairo_t* cr, int width, in
 {
 	if (drawing_area_callback != NULL)
 	{
-		drawing_area_callback(cr, width, height, _drawing_area_get_core(drawing_area));
+		drawing_area_t data = (drawing_area_t) user_data;
+		union _drawing_area_event e = {0};
+		e.draw.drawing_area = drawing_area;
+		e.draw.width = width;
+		e.draw.height = height;
+		e.draw.cr = cr;
+		drawing_area_callback(DAE_DRAW, data, &e);
+	}
+}
+
+void _drawing_area_mouse_button_pressed(GtkGestureClick* self, gint n_press, gdouble x, gdouble y, gpointer user_data)
+{
+	if (drawing_area_callback != NULL)
+	{
+		drawing_area_t data = (drawing_area_t) user_data;
+		union _drawing_area_event e = {0};
+		e.mouse_left_click_event.drawing_area = GTK_DRAWING_AREA(data->drawing_area);
+		e.mouse_left_click_event.x = x;
+		e.mouse_left_click_event.y = y;
+		e.mouse_left_click_event.n = n_press;
+		drawing_area_callback(DAE_MOUSE_CLICK_LEFT, data, &e);
 	}
 }
 
@@ -22,6 +42,9 @@ GtkWidget* drawing_area_create(uint32_t id, uint32_t width, uint32_t height, voi
 	core->user_data = user_data;
     gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(drawing_area), _drawing_area_draw, core, NULL);
     gtk_widget_set_size_request(drawing_area, width, height);
+    GtkGesture* mouse_click = gtk_gesture_click_new();
+    gtk_widget_add_controller(GTK_WIDGET(drawing_area), GTK_EVENT_CONTROLLER(mouse_click));
+    g_signal_connect(GTK_GESTURE(mouse_click), "pressed", G_CALLBACK(_drawing_area_mouse_button_pressed), core);
     return drawing_area;
 }
 
