48195fb778cb5013f8e21b86862588440ff8dec8
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2024-05-12 00:29:19 +0200

    ADD bullshit

--- src/gui_application.c
+++ src/gui_application.c
@@ -31,7 +31,7 @@ void _gui_application_shutdown(GApplication* app, gpointer user_data)
 int32_t gui_application_run(const char* name, int argc, char **argv, void* user_data)
 {
 	static struct _application core;
-	core.app = gtk_application_new(name, G_APPLICATION_FLAGS_NONE);
+	core.app = gtk_application_new(name, G_APPLICATION_DEFAULT_FLAGS);
 	core.user_data = user_data;
     g_signal_connect(core.app, "activate", G_CALLBACK(_gui_application_activate), &core);
     g_signal_connect(core.app, "startup", G_CALLBACK(_gui_application_startup), &core);
