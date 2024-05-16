72b2087eb0f1c42ef684e66e230479689f58eba6
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2024-05-16 11:55:33 +0200

    ADD extremly hard deep shit

--- gui_main_window.c
+++ gui_main_window.c
@@ -10,13 +10,14 @@ extern void gui_main_window_action_callback(GSimpleAction* simple_action, GVaria
 
 GMenu* _gui_main_window_create_menu_bar(GtkApplication* app, GtkApplicationWindow* window);
 void _gui_main_window_add_action(GtkApplication* app, const char* action_name, gui_main_window_t data);
+gui_main_window_t _gui_main_window_get_core(GtkWidget* main_window);
 
 gboolean _gui_main_window_key_pressed(GtkEventControllerKey* self, guint keyval, guint keycode, GdkModifierType state, gpointer user_data)
 {
     gboolean handled = FALSE;
 	if (gui_main_window_callback != NULL)
 	{
-		union _main_window_event e = {0};
+		union main_window_event e = {0};
 		e.key_pressed.keyval = keyval;
 		logging_log_message("key pressed event begin...", true);
 		gui_main_window_callback(MWE_KEY_PRESSED, (gui_main_window_t) user_data, &e);
@@ -30,7 +31,7 @@ void _gui_main_window_key_released(GtkEventControllerKey* self, guint keyval, gu
 {
 	if (gui_main_window_callback != NULL)
 	{
-		union _main_window_event e = {0};
+		union main_window_event e = {0};
 		e.key_released.keyval = keyval;
 		logging_log_message("key released event begin...", true);
 		gui_main_window_callback(MWE_KEY_RELEASED, (gui_main_window_t) user_data, &e);
@@ -43,7 +44,7 @@ gboolean _gui_main_window_close_request(GtkWindow* self, gpointer user_data)
 	gboolean close = FALSE;
 	if (gui_main_window_callback != NULL)
 	{
-		union _main_window_event e = {0};
+		union main_window_event e = {0};
 		gui_main_window_callback(MWE_CLOSE_REQUEST, (gui_main_window_t) user_data, &e);
 		close = e.close_request.close;
 	}
@@ -75,56 +76,70 @@ GMenu* _gui_main_window_create_menu_bar(GtkApplication* app, GtkApplicationWindo
     return menu_bar;
 }
 
+gui_main_window_t _gui_main_window_get_core(GtkWidget* main_window)
+{
+	return (gui_main_window_t) g_object_get_data(G_OBJECT(main_window), "core");
+}
+
 /*------------------------------------------------- PUBLIC ------------------------------------------------------*/
 
 void gui_main_window_create(GtkApplication* app, uint32_t width_pix, uint32_t height_pix, void* user_data, bool show_menu)
 {
-	static struct gui_main_window core = {0};
-	core.user_data = user_data;
-	core.app = app;
-	core.main_window = gtk_application_window_new(app);
+	GtkWidget* main_window = gtk_application_window_new(app);
+	g_object_set_data(G_OBJECT(main_window), "core", malloc(sizeof(struct gui_main_window)));
+	gui_main_window_t core = _gui_main_window_get_core(main_window);
+	core->main_window = main_window;
+	core->user_data = user_data;
+	core->app = app;
 	#ifdef USE_GTK3
 	core.keyboard_controller = gtk_event_controller_key_new(core.main_window);
 	#else
-	core.keyboard_controller = gtk_event_controller_key_new();
-    gtk_widget_add_controller(core.main_window, core.keyboard_controller);
+	core->keyboard_controller = gtk_event_controller_key_new();
+    gtk_widget_add_controller(core->main_window, core->keyboard_controller);
 	#endif
-	core.menu_bar = _gui_main_window_create_menu_bar(app, GTK_APPLICATION_WINDOW(core.main_window));
-    core.file_menu = gui_main_window_create_sub_menu(core.menu_bar, "File");
+	core->menu_bar = _gui_main_window_create_menu_bar(app, GTK_APPLICATION_WINDOW(core->main_window));
+    core->file_menu = gui_main_window_create_sub_menu(core->menu_bar, "File");
 	
-	gui_main_window_add_sub_menu_item(core.file_menu, "Exit", "exit", &core);
-	gtk_application_window_set_show_menubar(GTK_APPLICATION_WINDOW(core.main_window), show_menu ? TRUE : FALSE);
-	gtk_window_set_default_size(GTK_WINDOW(core.main_window), width_pix, height_pix);
+	gui_main_window_add_sub_menu_item(core->file_menu, "Exit", "exit", core);
+	gtk_application_window_set_show_menubar(GTK_APPLICATION_WINDOW(core->main_window), show_menu ? TRUE : FALSE);
+	gtk_window_set_default_size(GTK_WINDOW(core->main_window), width_pix, height_pix);
 
     //gtk_widget_add_controller(core.main_window, core.keyboard_controller);
-    g_signal_connect(core.keyboard_controller, "key-pressed", G_CALLBACK(_gui_main_window_key_pressed), &core);
-    g_signal_connect(core.keyboard_controller, "key-released", G_CALLBACK(_gui_main_window_key_released), &core);
+    g_signal_connect(core->keyboard_controller, "key-pressed", G_CALLBACK(_gui_main_window_key_pressed), core);
+    g_signal_connect(core->keyboard_controller, "key-released", G_CALLBACK(_gui_main_window_key_released), core);
 	#ifdef USE_GTK3
 	g_signal_connect(G_OBJECT(core.main_window), "delete-event", G_CALLBACK(_gui_main_window_close_request), &core);
 	#else
-    g_signal_connect(core.main_window, "close-request", G_CALLBACK(_gui_main_window_close_request), &core);
+    g_signal_connect(core->main_window, "close-request", G_CALLBACK(_gui_main_window_close_request), core);
 	#endif
 
 	if (gui_main_window_callback != NULL)
 	{
-		union _main_window_event e = {0};
+		union main_window_event e = {0};
 		logging_log_message("main window design phase begin.", true);
-		gui_main_window_callback(MWE_BEFORE_PRESENT, &core, &e);
+		gui_main_window_callback(MWE_BEFORE_PRESENT, core, &e);
 		logging_log_message("main window design phase end.", true);
 	}
 
 	#ifdef USE_GTK3
 	gtk_widget_show_all(core.main_window);
 	#endif
-    gtk_window_present(GTK_WINDOW(core.main_window));
+    gtk_window_present(GTK_WINDOW(core->main_window));
 
 	if (gui_main_window_callback != NULL)
 	{
-		union _main_window_event e = {0};
+		union main_window_event e = {0};
 		logging_log_message("main window initializing phase begin.", true);
-		gui_main_window_callback(MWE_AFTER_PRESENT, &core, &e);
+		gui_main_window_callback(MWE_AFTER_PRESENT, core, &e);
 		logging_log_message("main window initializing phase end.", true);
 	}
+}
+
+void gui_main_window_destroy(GtkWidget* main_window)
+{
+	free(_gui_main_window_get_core(main_window));
+	g_object_set_data(G_OBJECT(main_window), "core", NULL);
+	g_object_unref(G_OBJECT(main_window));
 }
 
 GMenu* gui_main_window_create_sub_menu(GMenu* menu_bar, const char* sub_menu_name)
