ce21fc52dc74b85e93203e45c58d8a0bf2da1c98
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2024-06-14 18:46:55 +0200

    ADD regular expressions

--- text.c
+++ text.c
@@ -1,5 +1,7 @@
 #include "text.h"
 
+#include <regex.h>
+
 /*------------------------------------------------- PRIVATE ------------------------------------------------------*/
 
 extern void _gui_add_widget_to_internal_list(GtkWidget* widget);
@@ -10,19 +12,26 @@ static void _gui_text_changed(GtkEditable* self, gpointer user_data)
     gui_text_t data = (gui_text_t) user_data;
     GtkEntryBuffer* buffer = gtk_text_get_buffer(GTK_TEXT(self));
     const char* string = gtk_entry_buffer_get_text(buffer);
-    uint32_t string_length = strlen(string);
-    for (uint32_t i = 0; i < string_length; ++i)
+    size_t string_length = strlen(string);
+    regex_t compiled_expression = {0};
+    if (regcomp(&compiled_expression, data->regular_expression, REG_EXTENDED) == 0)
     {
-        if (strchr(data->white_list, string[i]) == NULL)
+        regmatch_t pmatch[1] = {0};
+        regexec(&compiled_expression, string, 1, pmatch, 0);
+        
+        regoff_t regex_length = pmatch[0].rm_eo - pmatch[0].rm_so;
+        if (string_length != regex_length)
         {
-            gtk_entry_buffer_delete_text(buffer, i, 1);
+            gtk_entry_buffer_delete_text(buffer, regex_length, 1);
         }
+
+        regfree(&compiled_expression);
     }
 }
 
 /*------------------------------------------------- PUBLIC ------------------------------------------------------*/
 
-GtkWidget* gui_text_create(uint32_t id, float alignment, const char* white_list, const char* value, void* user_data)
+GtkWidget* gui_text_create(uint32_t id, float alignment, const char* regular_expression, const char* value, void* user_data)
 {
     GtkWidget* text = gtk_text_new();
 	g_object_set_data(G_OBJECT(text), "core", malloc(sizeof(struct _gui_text)));
@@ -30,7 +39,7 @@ GtkWidget* gui_text_create(uint32_t id, float alignment, const char* white_list,
     gtk_editable_set_text(GTK_EDITABLE(text), value);
     gui_text_t core = _gui_get_core(text);
     core->text = text;
-    core->white_list = white_list;
+    core->regular_expression = regular_expression;
     core->id = id;
     core->user_data = user_data;
     g_signal_connect(GTK_EDITABLE(text), "changed", G_CALLBACK(_gui_text_changed), core);
