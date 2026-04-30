0e6a0766a91745791001db91af7f184eaff16f11
Dipl.-Ing. (FH) Michael Czybor <Michael.Czybor@Live.de>
2026-04-30 23:14:56 +0200

    ...

--- gl.c
+++ gl.c
@@ -8,6 +8,13 @@ callback_declaration(void, gui_gl(gui_gl_t core, gui_event_t e));
 protected_import(void*, _gui_get_core(GtkWidget*));
 protected_import(void, _gui_add_widget_to_internal_list(GtkWidget*));
 
+private gboolean _gl_tick_cb(GtkWidget* widget, GdkFrameClock* clock, gpointer user_data)
+{
+    gui_gl_t core = user_data;
+    gtk_gl_area_queue_render(GTK_GL_AREA(widget));
+    return G_SOURCE_CONTINUE; // Loop weiterlaufen lassen
+}
+
 private gboolean _gui_gl_render(GtkGLArea *area, GdkGLContext *context, gpointer user_data)
 {
 	gui_gl_t core = user_data;
@@ -16,7 +23,13 @@ private gboolean _gui_gl_render(GtkGLArea *area, GdkGLContext *context, gpointer
 
 	if (gui_gl != NULL)
 	{
-        struct gui_event e = {0};
+		if (core->render_tick == false)
+		{
+			gtk_widget_add_tick_callback(GTK_WIDGET(core->gl_area), _gl_tick_cb, core, NULL);
+			core->render_tick = true;
+		}
+
+		struct gui_event e = {0};
         e.type = GE_GL_RENDER;
 		gui_gl(core, &e);
 	}
@@ -50,6 +63,7 @@ GtkWidget* gui_gl_create(GtkWidget* parent, gpointer user_data)
     gui_gl_t core = _gui_get_core(gl);
 	core->gl_area = gl;
 	core->user_data = user_data;
+	core->render_tick = false;
 	gtk_gl_area_set_has_depth_buffer(GTK_GL_AREA(gl), FALSE);
 	g_signal_connect(gl, "render", G_CALLBACK(_gui_gl_render), core);
 	g_signal_connect(gl, "realize", G_CALLBACK(_gui_gl_realize), core);
--- gl.h
+++ gl.h
@@ -1,11 +1,13 @@
 #pragma once
 
 #include <gtk/gtk.h>
+#include <epoxy/gl.h>
 
 typedef struct _gui_gl
 {
 	GtkWidget* gl_area;
 	gpointer user_data;
+	bool render_tick;
 } *gui_gl_t;
 
 GtkWidget* gui_gl_create(GtkWidget* parent, gpointer user_data);
